95fef2aba3ea96e2b9af8fb8da5344da62bbedd5
Oleg Nayanov <oleg.nayanov@dualbootpartners.com>
2021-06-09 19:40:36 +0400

    added solutions for arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,12 +1,43 @@
 module Exercise
   module Arrays
     class << self
-      def replace(array)
-        array
+      def find_max(arr)
+        max = arr[0]
+        arr.each do |i|
+          max = i if i > max
+        end
+        max
       end
 
-      def search(_array, _query)
-        0
+      def replace(array)
+        max = find_max(array)
+        result = []
+
+        array.each do |i|
+          result << if i.positive?
+                      max
+                    else
+                      i
+                    end
+        end
+
+        result
+      end
+
+      def search(array, element, index = 0)
+        middle = (array.size / 2).round
+
+        return -1 if array.empty? || array.size == 1 && array[0] != element
+
+        return index if array[0] == element
+
+        if element >= array[middle]
+          right_side = array[middle, array.size]
+          return search(right_side, element, index + middle)
+        end
+
+        left_side = array[0, middle]
+        search(left_side, element, index)
       end
     end
   end
